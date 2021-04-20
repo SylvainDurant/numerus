277f9e38cdf83c2149fc434bd94e5d0af0d30afa
SylvainDurant <71309860+SylvainDurant@users.noreply.github.com>
2021-04-20 18:37:12 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -10,18 +10,18 @@ window.onload = () => {
 
 ///// BEFORE INSTALL PROMPT /////
 // Initialize deferredPrompt for use later to show browser install prompt.
-let deferredPrompt;
+// let deferredPrompt;
 
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevent the mini-infobar from appearing on mobile
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-  // Update UI notify the user they can install the PWA
-  //   showInstallPromotion();
-  // Optionally, send analytics event that PWA install promo was shown.
-  console.log(`'beforeinstallprompt' event was fired.`);
-});
+// window.addEventListener('beforeinstallprompt', (e) => {
+//   // Prevent the mini-infobar from appearing on mobile
+//   e.preventDefault();
+//   // Stash the event so it can be triggered later.
+//   deferredPrompt = e;
+//   // Update UI notify the user they can install the PWA
+//   //   showInstallPromotion();
+//   // Optionally, send analytics event that PWA install promo was shown.
+//   console.log(`'beforeinstallprompt' event was fired.`);
+// });
 /////////////////////////////////
 
 const arabianInput = document.getElementById("arabianInput");
