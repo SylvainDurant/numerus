8fa8c1628204e1e540cf6d2f5ed83f9275b40c09
SylvainDurant <71309860+SylvainDurant@users.noreply.github.com>
2021-04-21 11:49:37 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -42,8 +42,8 @@ const showInstallButton = () => {
 }
 
 window.addEventListener('beforeinstallprompt', (e) => {
-    // Prevent the mini-infobar from appearing on mobile
-    e.preventDefault();
+//     // Prevent the mini-infobar from appearing on mobile
+//     e.preventDefault();
 
     // Stash the event so it can be triggered later.
     deferredPrompt = e;
