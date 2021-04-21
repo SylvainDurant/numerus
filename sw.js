809be02750cd4c0e29cf3a5df885f6ebd342b22e
SylvainDurant <71309860+SylvainDurant@users.noreply.github.com>
2021-04-21 15:24:14 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,11 +7,17 @@ let filesToCache = [
 
 /* Start the service worker and cache all of the app's content */
 self.addEventListener('install', (e) => {
+  console.log("service worker has been installed");
+  
   e.waitUntil(
     caches.open(cacheName).then(function(cache) {
       return cache.addAll(filesToCache);
     })
   );
+});
+
+self.addEventListener('activate', (e) => {
+  console.log("service worker has been activated");
 });
 
 /* Serve cached content when offline */
