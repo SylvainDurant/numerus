50335721f423e8b6f0f80707a491995471d33cd3
SylvainDurant <71309860+SylvainDurant@users.noreply.github.com>
2021-04-21 14:43:15 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,7 +3,9 @@ window.onload = () => {
     'use strict';
 
     if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('/sw.js');
+        navigator.serviceWorker.register('/sw.js')
+            .then((reg) => console.log("service worker registered", reg))
+            .catch((err) => console.log("service worker not supported", err));
     }
 }
 //////////////////////////
