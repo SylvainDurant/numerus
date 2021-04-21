a2c4b60d31bb030d946cb7884dfab753e6bf93d7
SylvainDurant <71309860+SylvainDurant@users.noreply.github.com>
2021-04-21 15:00:15 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 ///// SERVICE WORKER /////
-window.onload = () => {
-    'use strict';
+// window.onload = () => {
+//     'use strict';
 
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('./sw.js');
-    }
-}
+//     if ('serviceWorker' in navigator) {
+//       navigator.serviceWorker.register('./sw.js');
+//     }
+// }
 //////////////////////////
 
 ///// BEFORE INSTALL PROMPT /////
