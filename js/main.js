7c66de5d57697117a1e6de409eaf552cfc9739f7
SylvainDurant <71309860+SylvainDurant@users.noreply.github.com>
2021-04-23 09:36:13 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,36 +1,44 @@
 ///// BEFORE INSTALL PROMPT /////
 // Initialize deferredPrompt for use later to show browser install prompt.
-// let deferredPrompt;
+let deferredPrompt;
 
-// const showInstallButton = () => {
-//     document.getElementById("installButton").addEventListener("click", async () => {
-//         // Show the install prompt
-//         deferredPrompt.prompt();
+window.addEventListener('beforeinstallprompt', (evt) => {
+    // Stash the event so it can be triggered later.
+    deferredPrompt = evt;
 
-//         // Wait for the user to respond to the prompt
-//         const { outcome } = await deferredPrompt.userChoice;
+    // creating install button
+    console.log("beforeinstallprompt detected");
+    showInstallButton();
+});
 
-//         // Optionally, send analytics event with outcome of user choice
-//         if (outcome === "accepted") {
-//             let thanks = document.getElementById("message");
-//             thanks.innerHTML = "Thank you for installing my app!";
-//         }
-//         console.log(`User response to the install prompt: ${outcome}`);
+const showInstallButton = () => {
+    const installContainer = document.getElementById("installButtonContainer");
 
-//         // We've used the prompt, and can't use it again, throw it away
-//         deferredPrompt = null;
-//     });   
-// }
+    let installButton = document.createElement("button");
+    installButton.id = "installButton";
+    installButton.innerHTML = "Install me";
 
-// window.addEventListener('beforeinstallprompt', (e) => {
-//     // Stash the event so it can be triggered later.
-//     deferredPrompt = e;
+    installContainer.appendChild(installButton);
 
-//     // creating install button
-//     console.log("beforeinstallprompt detected");
-//     console.log(deferredPrompt);
-//     showInstallButton();
-// });
+    installButton.addEventListener("click", async () => {
+        // Show the install prompt
+        deferredPrompt.prompt();
+
+        // Wait for the user to respond to the prompt
+        const { outcome } = await deferredPrompt.userChoice;
+
+        // Optionally, send analytics event with outcome of user choice
+        if (outcome === "accepted") {
+            let thanks = document.getElementById("message");
+            thanks.innerHTML = "Thank you for installing my app!";
+        }
+        console.log(`User response to the install prompt: ${outcome}`);
+
+        // We've used the prompt, and can't use it again, throw it away
+        deferredPrompt = null;
+        installButton.remove();
+    });   
+}
 /////////////////////////////////
 
 const arabianInput = document.getElementById("arabianInput");
