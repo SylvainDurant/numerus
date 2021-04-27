ce120c1382b47424b5e4149200a11e9d4e0b4f8c
Sylvain <sy.durant@hotmail.com>
2021-04-27 08:57:36 +0200

    migration

---
+++ sw.js
@@ -0,0 +1,77 @@
+const staticCache = "numerus-static-v1.5.2";
+const dynamicCache = "numerus-dynamic-v1";
+const assets = [
+  "/index.html",
+  "/favicon.ico",
+  "/pages/fallback.html",
+  "/style/style.css",
+  "/js/main.js",
+  "/js/insatllPrompt.js",
+  "/js/app.js",
+  "/images/background.jpg"
+];
+
+///// LIMIT CACHE SIZE
+const limitCacheSize = (name, size) => {
+  caches.open(name).then(cache => {
+    cache.keys().then(keys => {
+      if (keys.length > size) {
+        cache.delete(keys[0])
+        .then(limitCacheSize(name, size));
+      }
+    })
+  })
+}
+
+///// INSTALL SERVICE WORKER
+self.addEventListener("install", evt => {
+  console.log("service worker has been installed");
+  
+  // cache the static assets
+  evt.waitUntil(
+    caches.open(staticCache).then(cache => {
+      console.log("caching static assets");
+      cache.addAll(assets);
+    })
+  );
+});
+
+///// ACTIVATE SERVICE WORKER
+self.addEventListener("activate", evt => {
+  console.log("service worker has been activated");
+
+  // deleting old caches
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key => key !== staticCache && key !== dynamicCache)
+        .map(key => caches.delete(key)))
+    })
+  )
+});
+
+///// FETCH EVENTS
+// looks what the app is fetching
+self.addEventListener("fetch", evt => {
+  // looks if the fetched asset is already in the caches
+  evt.respondWith(
+    caches.match(evt.request).then(response => {
+      return response || fetch(evt.request).then( fetchResponse => {
+        // if it's not in the caches, cache it in the dynamic cache 
+        return caches.open(dynamicCache).then(cache => {
+          // the fetchResponse is needed for the return, so it cache a copy of the fetchResponse
+          cache.put(evt.request.url, fetchResponse.clone());
+          // check if the dynamic cache is full
+          limitCacheSize(dynamicCache,10);
+          return fetchResponse;
+        })
+      });
+      // if the fetch fail (asset not in the caches while offline)
+    }).catch(() => {
+      // if it was a page fetch then redirect to the fallback page
+      if (evt.request.url.indexOf(".html") > -1) {
+        return caches.match("/pages/fallback.html");
+      }
+    })
+  );
+});
